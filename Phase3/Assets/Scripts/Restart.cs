d1e0fd558b720f6b1f7f10362a612495de141826
Unknown <josephine.garcia2020@gmail.com>
2019-04-29 12:51:37 -0400

    game to minigame transition works

--- Phase3/Assets/Scripts/Restart.cs
+++ Phase3/Assets/Scripts/Restart.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Fungus;
+
 
 public class Restart : MonoBehaviour {
 
-
-	void Update () {
-        if (Input.GetKeyDown(KeyCode.R)){
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.C)){
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); //If want restart, uncomment this and change .C to .R
+            //flowchart.ExecuteBlock("Start");
+            SceneManager.LoadScene(20);
         }
 	}
 }
